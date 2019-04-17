56f2e8fbcac72b802c4991120785137fc7173441
Ajay Raj Rijal <rijal.ajayraj@gmail.com>
2019-04-17 16:45:58 -0400

    Create maxRangeSums.js

---
+++ maxRangeSums.js
@@ -0,0 +1,17 @@
+a = (arr) => {
+    let max = 0;
+    let maxSoFar = 0;
+    for (let a of arr) {
+       max = max + a;
+       if (max > maxSoFar) {
+       		maxSoFar
+          a([7, -3, -10, 4, 2, 8, -2, 4, -5, -6])max;
+		}
+       if (max < 0) {
+		max = 0;
+       }
+    }
+    return maxSoFar;
+}
+
+a([7, -3, -10, 4, 2, 8, -2, 4, -5, -6])
